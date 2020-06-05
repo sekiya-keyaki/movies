01822836c7e4107fd3b60d642319596056a1a189
Sekiya Hikaru <sekiyahika@icloud.com>
2020-06-05 15:21:24 +0900

    画像と検索フォームの作成

--- laravel_project/resources/views/top.blade.php
+++ laravel_project/resources/views/top.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  
-  
+<div class="container">
+<img src="storage/e1210e3187d3301afcc77b53d2c9291e_t.jpg"> 
+
+
+<form method="GET">
+<input name="body" type="text" value=""/>
+<button type="submit">検索</button>
+</form>
+</div>
 @endsection
