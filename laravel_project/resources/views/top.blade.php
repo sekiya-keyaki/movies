37ea658152cedbeffea5fcb7d62a660d78316e38
Sekiya Hikaru <sekiyahika@icloud.com>
2020-06-05 17:04:48 +0900

    トップページを修正

--- laravel_project/resources/views/top.blade.php
+++ laravel_project/resources/views/top.blade.php
@@ -2,12 +2,22 @@
 
 @section('content')
 <div class="container">
-<img src="storage/e1210e3187d3301afcc77b53d2c9291e_t.jpg"> 
+<style>
+.image_size_m{
+  width: 100%;
+}
+</style>
 
+<div style="text-align: center;width:100%">
+<img src="storage/e1210e3187d3301afcc77b53d2c9291e_t.jpg" class="image_size_m"> 
+</div>
 
+<div style="text-align: center;margin: 50px">
 <form method="GET">
-<input name="body" type="text" value=""/>
+<input type="seach" placeholder="キーワードを入力"/>
 <button type="submit">検索</button>
 </form>
 </div>
+</div>
+
 @endsection
