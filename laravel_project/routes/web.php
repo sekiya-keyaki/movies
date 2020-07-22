ac6e3f69c55c83bd85eb0fa0a495346c19ab78e1
Sekiya Hikaru <sekiyahika@icloud.com>
2020-07-22 09:53:03 +0900

    トップページを/top → / に変更する

--- laravel_project/routes/web.php
+++ laravel_project/routes/web.php
@@ -13,14 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 
 
-Route::get('top', 'MovieController@top')->name('movie.top');
+
+Route::get('/', 'MovieController@top')->name('movie.top');
 Route::get('movies', 'MovieController@index')->name('movie.index');
 Route::get('show/{id}', 'MovieController@show')->name('movie.show');
 Route::get('review/{movieId}', 'MovieController@review')->name('movie.review');
